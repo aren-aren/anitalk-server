5a7a6eea17e45b8108f5cb0075d75bc22fa272d4
dongil <az330015@gmail.com>
2024-06-08 19:17:38 +0900

    프로젝트 생성

---
+++ src/test/java/com/anitalk/app/AnitalkServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anitalk.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnitalkServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
