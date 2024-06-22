89b11e42f58d9999a1bf9506f6c6175b75d4e085
dongil <az330015@gmail.com>
2024-06-22 17:57:06 +0900

    fix : cors allow all

--- src/main/java/com/anitalk/app/config/WebConfig.java
+++ src/main/java/com/anitalk/app/config/WebConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOriginPatterns("*")
                 .allowedMethods("*")
                 .allowCredentials(true);
