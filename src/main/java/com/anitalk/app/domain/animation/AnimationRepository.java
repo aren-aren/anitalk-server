d7918a695ccca263215ce560da842b61a0b5dfbe
dongil <az330015@gmail.com>
2024-06-23 21:01:06 +0900

    fix : 애니메이션 정렬기준 수정

--- src/main/java/com/anitalk/app/domain/animation/AnimationRepository.java
+++ src/main/java/com/anitalk/app/domain/animation/AnimationRepository.java
@@ -12,9 +12,13 @@ import org.springframework.stereotype.Repository;
 public interface AnimationRepository extends JpaRepository<AnimationEntity, Long> {
     AnimationEntity findByName(String name);
 
-    @Query(
-            value = "select distinct ani from AnimationEntity ani join fetch FavoriteEntity fav on ani.id = fav.animation.id where fav.user.id = :userId"
-    )
+    @Query("""
+        select distinct ani
+        from AnimationEntity ani
+            join fetch FavoriteEntity fav on ani.id = fav.animation.id
+        where fav.user.id = :userId
+        order by ani.currentDate desc
+    """)
     Page<AnimationEntity> findAllByUserId(Long userId, Pageable pageable);
 
     @Query("""
