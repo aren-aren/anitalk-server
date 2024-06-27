62f546e5e6b2eeb362721470d7dc7904143f3b76
dongil <az330015@gmail.com>
2024-06-27 22:28:58 +0900

    fix : ip null일때 오류 수정

--- src/main/java/com/anitalk/app/utils/IpFormatter.java
+++ src/main/java/com/anitalk/app/utils/IpFormatter.java
@@ -2,6 +2,8 @@ package com.anitalk.app.utils;
 
 public class IpFormatter {
     public static String format(String ip){
+        if(ip == null) return ip;
+
         String[] ips = ip.split("\\.");
         String result = ip;
         if(ips.length > 1){
